ffe52da3677bafd8d4bd4dc25f22ae2f1013ddd9
Dante Martinelli <dantemm13@gmail.com>
2019-06-27 11:03:19 -0300

    Implementação dos serviços de heroi e categoria

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppRequestService } from './../app-request.service';
+
+import { Category } from 'src/app/models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(
+    private _appRequestService: AppRequestService
+  ) { }
+
+  public getCategories(): Observable<any> {
+    return this._appRequestService.get('/Category', {})
+  }
+
+  public postCategory(category: Category): Observable<any> {
+    return this._appRequestService.post('/Category', category)
+  }
+
+  public deleteCategory(categoryId: string): Observable<any> {
+    return this._appRequestService.delete('/Category', categoryId)
+  }
+
+  public putCategory(category: Category, categoryId: string): Observable<any> {
+    return this._appRequestService.put('/Heroes', category, categoryId)
+  }
 }
--- src/app/services/heroes/heroes.service.ts
+++ src/app/services/heroes/heroes.service.ts
@@ -2,14 +2,30 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppRequestService } from './../app-request.service';
 
+import { Hero } from './../../models/hero.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  constructor() { }
+  constructor(
+    private _appRequestService: AppRequestService
+  ) { }
 
-  public getHeroes(): void {
+  public getHeroes(): Observable<any> {
+    return this._appRequestService.get('/Heroes', {})
+  }
 
+  public postHero(hero: Hero): Observable<any> {
+    return this._appRequestService.post('/Heroes', hero)
+  }
+
+  public deleteHero(heroId: string): Observable<any> {
+    return this._appRequestService.delete('/Heroes', heroId)
+  }
+
+  public putHero(hero: Hero, heroId: string): Observable<any> {
+    return this._appRequestService.put('/Heroes', hero, heroId)
   }
 }
